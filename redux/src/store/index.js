15c3a97068241ea164dfcfc6352e1c33e5980b3a
estherlardze <estherlardze8@gmail.com>
2023-11-16 15:27:45 +0000

    no change

--- redux/src/store/index.js
+++ redux/src/store/index.js
@@ -3,13 +3,16 @@ import {configureStore, createSlice} from '@reduxjs/toolkit'
 const counterSlice = createSlice({
   name: 'counter',
   initialState: { counter:0 },
+
   reducers:{
     increment(state, action){
        state.counter ++
     },
+
     decrement(state, action){
        state.counter --
     },
+    
     addBy(state, action){
       state.counter += action.payload
     }
