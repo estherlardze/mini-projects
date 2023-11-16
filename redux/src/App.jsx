0184950d6a5f40c36d1aea9b4aa31756b01b2695
estherlardze <estherlardze8@gmail.com>
2023-11-16 16:03:56 +0000

    modify code

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,28 +1,41 @@
 import { useSelector, useDispatch } from "react-redux"
-import { actions } from "./store/index"
+import { increment, decrement, reset, addByAmount } from "./store/index"
+import { useState } from "react"
 
 function App() {
+   const [addAmount, setAddAmount] = useState(0)
+   const counter = useSelector((state) => state.counter)
+   const dispatch = useDispatch()
 
-  const counter = useSelector((state) => state.counter)
-  const dispatch = useDispatch()
+   const checkForNum = Number(addAmount) || 0
 
-  const increment = ( ) => {
-    dispatch(actions.increment())
-  }
-  const decrement = ( ) => {
-    dispatch(actions.decrement())
-  }
-  const addBy = ( ) => {
-    dispatch(actions.addBy(10))
+  const resetAll = () => {
+    setAddAmount(0)
+    dispatch(reset())
   }
 
   return (
     <>
       <h1>Counter</h1>
-      <p>{counter}</p>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={addBy}>Add by 10</button>
+      <h2>{counter}</h2>
+
+      <div style={{display:"flex", gap:"20px"}}>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </div>
+
+      <input 
+        type="text" 
+        value={addAmount}
+        onChange={(e) => setAddAmount(e.target.value)}
+        style={{outline:"none", padding:"5px 0", margin:"20px 0", fontSize:"20px", width:"50px"}}
+      /> 
+
+     <div  style={{display:"flex", gap:"20px"}}>
+      <button onClick={resetAll}>Reset</button>
+      <button onClick={() => dispatch(addByAmount(checkForNum))}>Add by Amount</button>
+     </div>
+      
     </>
   )
 }
--- redux/src/store/index.js
+++ redux/src/store/index.js
@@ -2,24 +2,29 @@ import {configureStore, createSlice} from '@reduxjs/toolkit'
 
 const counterSlice = createSlice({
   name: 'counter',
-  initialState: { counter:0 },
+  initialState: { counter: 0 },
 
   reducers:{
-    increment(state, action){
+    increment(state){
        state.counter ++
     },
 
-    decrement(state, action){
+    decrement(state){
        state.counter --
     },
     
-    addBy(state, action){
+    addByAmount(state, action){
       state.counter += action.payload
+    },
+
+    reset(state){
+      state.counter = 0
     }
   }
 })
 
-export const actions = counterSlice.actions
+export const {increment, decrement, addByAmount, reset} = counterSlice.actions;
+
 const store = configureStore({
   reducer: counterSlice.reducer
 })
